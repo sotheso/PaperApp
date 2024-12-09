1cabffc978cfcd6ecf4de48ca1a04dccf75e735b
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-12-09 20:59:06 +0330

    Start App

--- PaperApp/PaperAppApp.swift
+++ PaperApp/PaperAppApp.swift
@@ -6,9 +6,24 @@
 //
 
 import SwiftUI
+import Firebase
+
 
 @main
 struct PaperAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    init() {
+        FirebaseApp.configure()
+
+        // تست اتصال به Firebase
+        if let app = FirebaseApp.app() {
+            print("Firebase is successfully configured: \(app.name)")
+        } else {
+            print("Firebase is NOT configured.")
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
