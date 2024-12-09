f8439e764db16472be316ee5945a2afd7007ecf0
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-12-09 13:24:30 +0330

    Initial Commit

---
+++ PaperApp/PaperAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperAppApp.swift
+//  PaperApp
+//
+//  Created by Sothesom on 19/09/1403.
+//
+
+import SwiftUI
+
+@main
+struct PaperAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
